5ad1aa35e350861ebf30dc4d445e633723bcb3ca
Rewan <samoylovsasha@gmail.com>
2015-09-28 22:02:44 +0300

    Add some part of homework

--- src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
@@ -7,57 +7,71 @@ public class TemperatureSeriesAnalysis {
 	public static final double MINIMAL_TEMPERATURE = -273.0;
 
 	private double[] tempsArr;
-	private int size = 0;
+	private int size;
 
+	public boolean checkArrayForOutOfRange(double ... temps) {
+		for(double x: temps) {
+			if(x < MINIMAL_TEMPERATURE) return  true;
+		}
+		
+		return false;
+	}
+	
     public TemperatureSeriesAnalysis() {
         tempsArr = new double[1000];
+		size = 0;
     }
     
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-		for(double x: temperatureSeries) {
-			if(x < MINIMAL_TEMPERATURE) throw new InputMismatchException();
-		}
+		if(checkArrayForOutOfRange(temperatureSeries)) throw new InputMismatchException();
 		
 		tempsArr = temperatureSeries.clone();
+		size = temperatureSeries.length;
     }
     
-    public double average(){        
+    public double average() {        
         return 0;
     }    
     
-    public double deviation(){
+    public double deviation() {
         return 0;
     }
     
-    public double min(){
+    public double min() {
         return 0;
     }
      
-    public double max(){
+    public double max() {
         return 0;
     }
     
-    public double findTempClosestToZero(){
+    public double findTempClosestToZero() {
         return 0;
     }
     
-    public double findTempClosestToValue(double tempValue){
+    public double findTempClosestToValue(double tempValue) {
         return 0;
 }
     
-    public double[] findTempsLessThen(double tempValue){
+    public double[] findTempsLessThen(double tempValue) {
         return null;
     }
     
-    public double[] findTempsGreaterThen(double tempValue){
+    public double[] findTempsGreaterThen(double tempValue) {
         return null;
     }
     
-    public TempSummaryStatistics summaryStatistics(){
+    public TempSummaryStatistics summaryStatistics() {
         return null;
     }
     
-    public int addTemps(double ... temps){
-        return 0;
+    public int addTemps(double ... temps) {
+		if(checkArrayForOutOfRange(temps)) throw new InputMismatchException();
+		
+		if(temps.length+size>tempsArr.length) {
+			
+		}
+		
+        return this.size;
     }
 }
--- src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
@@ -29,6 +29,33 @@ public class TemperatureSeriesAnalysisTest {
 		TemperatureSeriesAnalysis T = new TemperatureSeriesAnalysis(temps);
 	}
 	
+	@Test
+	public void testAddTempsWithEmptyArgs() {
+		double[] startTemps = {10.0, -270.0, -3.0, 352.0};
+		TemperatureSeriesAnalysis T = new TemperatureSeriesAnalysis(startTemps);
+		int expResult = 4;
+		int result = T.addTemps();
+		assertEquals(expResult, result);
+	}
+	
+	@Test(expected = InputMismatchException.class)
+	public void testAddTempsWithListWithElementOutOfRange() {
+		double[] startTemps = {10.0, -270.0, -3.0, 352.0};
+		TemperatureSeriesAnalysis T = new TemperatureSeriesAnalysis(startTemps);
+		double[] temps =  {1.0, -274.0};
+		int result = T.addTemps(temps);
+	}
+	
+	@Test
+	public void testAddTempsWitNormalList() {
+		double[] startTemps = {10.0, -270.0, -3.0, 352.0};
+		TemperatureSeriesAnalysis T = new TemperatureSeriesAnalysis(startTemps);
+		int expResult = 8;
+		double[] temps =  {52.0, -234.0, 34.3, -61.0};
+		int result = T.addTemps();
+		assertEquals(expResult, result);
+	}
+	
     @Test
     public void testAverage() {
         double[] temperatureSeries = {1.0, -5.0, 1.0, 5.0};
