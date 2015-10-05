786d52efe293f396d06dd27fe2f404f5c69b7df5
Rewan <samoylovsasha@gmail.com>
2015-10-05 17:36:04 +0300

    v1.8

--- src/main/java/ua/yandex/shad/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/yandex/shad/tempseries/TempSummaryStatistics.java
@@ -3,72 +3,72 @@ package ua.yandex.shad.tempseries;
 public class TempSummaryStatistics {
     public static final double EPSILON = 0.00001;
 
-	private final double avgTemp;
-	private final double devTemp;
-	private final double minTemp;
-	private final double maxTemp;
-	
-	public TempSummaryStatistics(double avg, double dev, 
-			double min, double max) {
-		avgTemp = avg;
-		devTemp = dev;
-		minTemp = min;
-		maxTemp = max;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		
-		if (obj == null) {
-			return false;
-		}
-		
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		
-		TempSummaryStatistics statistics = (TempSummaryStatistics) obj;
-		
-		if (Math.abs(avgTemp - statistics.getAvgTemp()) >= EPSILON) {
-			return false;
-		}
-		
-		if (Math.abs(devTemp - statistics.getDevTemp()) >= EPSILON) {
-			return false;
-		}
-		
-		if (Math.abs(minTemp - statistics.getMinTemp()) >= EPSILON) {
-			return false;
-		}
-		
-		if (Math.abs(maxTemp - statistics.getMaxTemp()) >= EPSILON) {
-			return false;
-		}
-		
-		return true;
-	}
-	
-	public double getAvgTemp() {
-		return this.avgTemp;
-	}
-	
-	public double getDevTemp() {
-		return this.devTemp;
-	}
-	
-	public double getMinTemp() {
-		return this.minTemp;
-	}
-	
-	public double getMaxTemp() {
-		return this.maxTemp;
-	}
-	
-	public int hashCode() {
-		assert false : "hashCode not designed";
-		return 42; // any arbitrary constant will do
-	}
+    private final double avgTemp;
+    private final double devTemp;
+    private final double minTemp;
+    private final double maxTemp;
+    
+    public TempSummaryStatistics(double avg, double dev, 
+            double min, double max) {
+        avgTemp = avg;
+        devTemp = dev;
+        minTemp = min;
+        maxTemp = max;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        
+        if (obj == null) {
+            return false;
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        TempSummaryStatistics statistics = (TempSummaryStatistics) obj;
+        
+        if (Math.abs(avgTemp - statistics.getAvgTemp()) >= EPSILON) {
+            return false;
+        }
+        
+        if (Math.abs(devTemp - statistics.getDevTemp()) >= EPSILON) {
+            return false;
+        }
+        
+        if (Math.abs(minTemp - statistics.getMinTemp()) >= EPSILON) {
+            return false;
+        }
+        
+        if (Math.abs(maxTemp - statistics.getMaxTemp()) >= EPSILON) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public double getAvgTemp() {
+        return this.avgTemp;
+    }
+    
+    public double getDevTemp() {
+        return this.devTemp;
+    }
+    
+    public double getMinTemp() {
+        return this.minTemp;
+    }
+    
+    public double getMaxTemp() {
+        return this.maxTemp;
+    }
+    
+    public int hashCode() {
+        assert false : "hashCode not designed";
+        return 42; // any arbitrary constant will do
+    }
 }
--- src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
@@ -4,229 +4,229 @@ import java.util.InputMismatchException;
 
 public class TemperatureSeriesAnalysis {
     public static final double EPSILON = 0.00001;
-	public static final double MINIMAL_TEMPERATURE = -273.0;
+    public static final double MINIMAL_TEMPERATURE = -273.0;
 
-	private double[] tempsArr;
-	private int size;
+    private double[] tempsArr;
+    private int size;
 
-	public TemperatureSeriesAnalysis() {
+    public TemperatureSeriesAnalysis() {
         tempsArr = new double[1];
-		size = 0;
+        size = 0;
     }
-	
-	public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-		checkInputMistmach(temperatureSeries);
-		tempsArr = temperatureSeries.clone();
-		size = temperatureSeries.length;
-		
-		if (temperatureSeries.length == 0) {
-			tempsArr = new double[1];
-		}
+    
+    public TemperatureSeriesAnalysis(double[] temperatureSeries) {
+        checkInputMistmach(temperatureSeries);
+        tempsArr = temperatureSeries.clone();
+        size = temperatureSeries.length;
+        
+        if (temperatureSeries.length == 0) {
+            tempsArr = new double[1];
+        }
     }
-	
-	public boolean checkArrayForOutOfRange(double ... temps) {
-		for (double x: temps) {
-			if (x < MINIMAL_TEMPERATURE) {
-				return  true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public void checkInputMistmach(double ... temps) {
-		if (checkArrayForOutOfRange(temps)) {
-			throw new InputMismatchException();
-		}
-	}
-	
-	public boolean checkEmpty() {
-		return size == 0;
-	}
-	
-	public void checkIllegalArgument() {
-		if (checkEmpty()) {
-			throw new IllegalArgumentException();
-		}
-	}
+    
+    public boolean checkArrayForOutOfRange(double ... temps) {
+        for (double x: temps) {
+            if (x < MINIMAL_TEMPERATURE) {
+                return  true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public void checkInputMistmach(double ... temps) {
+        if (checkArrayForOutOfRange(temps)) {
+            throw new InputMismatchException();
+        }
+    }
+    
+    public boolean checkEmpty() {
+        return size == 0;
+    }
+    
+    public void checkIllegalArgument() {
+        if (checkEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
     
     public double average() {
-		checkIllegalArgument();
-		
-		double sum = 0.0;
-		for (int i = 0; i < size; i++) {
-			sum += tempsArr[i];
-		}
-		
-		double ans = sum/size;
+        checkIllegalArgument();
+        
+        double sum = 0.0;
+        for (int i = 0; i < size; i++) {
+            sum += tempsArr[i];
+        }
+        
+        double ans = sum/size;
         return ans;
     }    
     
     public double deviation() {
         checkIllegalArgument();
-		
-		double averageTemp = average();
-		
-		double sum = 0.0;
-		for (int i = 0; i < size; i++) {
-			double x = tempsArr[i] - averageTemp;
-			sum += x*x;
-		}
-		
-		double ans = Math.sqrt(sum/size);
+        
+        double averageTemp = average();
+        
+        double sum = 0.0;
+        for (int i = 0; i < size; i++) {
+            double x = tempsArr[i] - averageTemp;
+            sum += x*x;
+        }
+        
+        double ans = Math.sqrt(sum/size);
         return ans;
     }
     
     public double min() {
         checkIllegalArgument();
-		
-		double min = tempsArr[0];
-		
-		for (int i = 1; i < size; i++) {
-			if (tempsArr[i] < min) {
-				min = tempsArr[i];
-			}
-		}
-		
+        
+        double min = tempsArr[0];
+        
+        for (int i = 1; i < size; i++) {
+            if (tempsArr[i] < min) {
+                min = tempsArr[i];
+            }
+        }
+        
         return min;
     }
      
     public double max() {
         checkIllegalArgument();
-		
-		double max = tempsArr[0];
-		
-		for (int i = 1; i < size; i++) {
-			if (tempsArr[i] > max) {
-				max = tempsArr[i];
-			}
-		}
-		
+        
+        double max = tempsArr[0];
+        
+        for (int i = 1; i < size; i++) {
+            if (tempsArr[i] > max) {
+                max = tempsArr[i];
+            }
+        }
+        
         return max;
     }
     
     public double findTempClosestToZero() {
         checkIllegalArgument();
-		
-		double ans = tempsArr[0];
-		
-		for (int i = 1; i < size; i++) {
-			if (Math.abs(tempsArr[i] - Math.abs(ans)) < EPSILON) {
-				ans = tempsArr[i];
-			}
-			if (Math.abs(tempsArr[i]) < Math.abs(ans)) {
-				ans = tempsArr[i];
-			}
-		}
-		
-		return ans;
+        
+        double ans = tempsArr[0];
+        
+        for (int i = 1; i < size; i++) {
+            if (Math.abs(tempsArr[i] - Math.abs(ans)) < EPSILON) {
+                ans = tempsArr[i];
+            }
+            if (Math.abs(tempsArr[i]) < Math.abs(ans)) {
+                ans = tempsArr[i];
+            }
+        }
+        
+        return ans;
     }
     
-	public void addToTemps(double tempValue) {
-		for (int i = 0; i < size; i++) {
-			tempsArr[i] += tempValue;
-		}
-	}
-	
+    public void addToTemps(double tempValue) {
+        for (int i = 0; i < size; i++) {
+            tempsArr[i] += tempValue;
+        }
+    }
+    
     public double findTempClosestToValue(double tempValue) {
         checkIllegalArgument();
-		
-		addToTemps(-tempValue);
-		double ans = findTempClosestToZero() + tempValue;
-		addToTemps(tempValue);
         
-		return ans;
-	}
+        addToTemps(-tempValue);
+        double ans = findTempClosestToZero() + tempValue;
+        addToTemps(tempValue);
+        
+        return ans;
+    }
     
     public double[] findTempsLessThen(double tempValue) {
-		checkIllegalArgument();
-		
-		double[] arr = new double[tempsArr.length];
-		int arrSize = 0;
-		for (int i = 0; i < size; i++) {
-			if (tempsArr[i] < tempValue) {
-				arr[arrSize++] = tempsArr[i];
-			}
-		}
-		
-		double[] ans = new double[arrSize];
-		
-		if (arrSize == 0) {
-			return new double[0];
-		}
-		
-		System.arraycopy(arr, 0, ans, 0, arrSize);
+        checkIllegalArgument();
+        
+        double[] arr = new double[tempsArr.length];
+        int arrSize = 0;
+        for (int i = 0; i < size; i++) {
+            if (tempsArr[i] < tempValue) {
+                arr[arrSize++] = tempsArr[i];
+            }
+        }
+        
+        double[] ans = new double[arrSize];
+        
+        if (arrSize == 0) {
+            return new double[0];
+        }
+        
+        System.arraycopy(arr, 0, ans, 0, arrSize);
         return ans;
     }
     
     public double[] findTempsGreaterThen(double tempValue) {
-		double[] less = findTempsLessThen(tempValue);
-		
-		if (less.length == 0) {
-			return tempsArr.clone();
-		}
-		
-		if (less.length == tempsArr.length) {
-			return new double[0];
-		}
-		
-		double[] ans = new double[tempsArr.length - less.length];
-		int ansSize = 0;
-		
-		for (int i = 0; i < size; i++) {
-			boolean was = false;
-			for (int j = 0; j < less.length; j++) {
-				if (Math.abs(tempsArr[i] - less[j]) < EPSILON) {
-					was = true;
-					break;
-				}
-			}
-			if (!was) {
-				ans[ansSize++] = tempsArr[i];
-			}
-		}
-		
+        double[] less = findTempsLessThen(tempValue);
+        
+        if (less.length == 0) {
+            return tempsArr.clone();
+        }
+        
+        if (less.length == tempsArr.length) {
+            return new double[0];
+        }
+        
+        double[] ans = new double[tempsArr.length - less.length];
+        int ansSize = 0;
+        
+        for (int i = 0; i < size; i++) {
+            boolean was = false;
+            for (int j = 0; j < less.length; j++) {
+                if (Math.abs(tempsArr[i] - less[j]) < EPSILON) {
+                    was = true;
+                    break;
+                }
+            }
+            if (!was) {
+                ans[ansSize++] = tempsArr[i];
+            }
+        }
+        
         return ans;
     }
     
     public TempSummaryStatistics summaryStatistics() {
-		checkIllegalArgument();
-		
-		double avg = average();
-		double dev = deviation();
-		double min = min();
-		double max = max();
-		
-		TempSummaryStatistics ans = 
-			new TempSummaryStatistics(avg, dev, min, max);
-		
+        checkIllegalArgument();
+        
+        double avg = average();
+        double dev = deviation();
+        double min = min();
+        double max = max();
+        
+        TempSummaryStatistics ans = 
+            new TempSummaryStatistics(avg, dev, min, max);
+        
         return ans;
     }
     
     public int addTemps(double ... temps) {
-		checkInputMistmach(temps);
-		
-		if (temps.length + size > tempsArr.length) {
-			int n = tempsArr.length;
-			
-			while (n < temps.length+size) {
-				n *= 2;
-			}
-			
-			double[] old = new double[tempsArr.length];
-			System.arraycopy(tempsArr, 0, old, 0, tempsArr.length);
-			
-			tempsArr = new double[n];
-			System.arraycopy(old, 0, tempsArr, 0, old.length);
-		}
-		
-		if (temps.length > 0) {
-			System.arraycopy(temps, 0, 
-				tempsArr, size, temps.length);
-		
-			size += temps.length;
-		}
-		
+        checkInputMistmach(temps);
+        
+        if (temps.length + size > tempsArr.length) {
+            int n = tempsArr.length;
+            
+            while (n < temps.length+size) {
+                n *= 2;
+            }
+            
+            double[] old = new double[tempsArr.length];
+            System.arraycopy(tempsArr, 0, old, 0, tempsArr.length);
+            
+            tempsArr = new double[n];
+            System.arraycopy(old, 0, tempsArr, 0, old.length);
+        }
+        
+        if (temps.length > 0) {
+            System.arraycopy(temps, 0, 
+                tempsArr, size, temps.length);
+        
+            size += temps.length;
+        }
+        
         return size;
     }
 }
